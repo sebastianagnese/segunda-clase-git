5c9c1a3cdbf562af69499c959616cae6af138bb0
alumno <alumno@lab6pc00>
2019-08-28 19:11:53 -0300

    main creado

---
+++ Segunda clase/src/Segunda clase.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Segunda.c
+ Author      : agn
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	prinf("Hola");
+	return EXIT_SUCCESS;
+}
