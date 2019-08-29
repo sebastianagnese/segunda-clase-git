b904621a49f296b56d55faa15a728871f78244a2
alumno <alumno@lab6pc00>
2019-08-28 21:29:23 -0300

    Cargada c/calcularMaxMin

--- Segunda clase/src/Segunda clase.c
+++ Segunda clase/src/Segunda clase.c
@@ -1,17 +1,49 @@
-/*
- ============================================================================
- Name        : Segunda.c
- Author      : agn
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int) {
-	prinf("Hola");
+int calculaMaxMin(int *nMax,int *nMin);
+
+int main(void)
+{
+	int nMax;
+	int nMin;
+
+	calculaMaxMin(&nMax,&nMin);
+
+	printf("\nEl mayor numero es %d y el menor numero es %d",nMax,nMin);
 	return 0;
 }
+
+int calculaMaxMin(int *nMax,int *nMin) {
+	int numero;
+	int flag;
+	int i;
+	flag = 0;
+	i = 1;
+
+	while (i < 8)
+	{
+		printf("\nIngrese un numero: ");
+		scanf("%d",&numero);
+
+		if (flag == 0 || numero > nMax)
+		{
+			*nMax = numero;
+		}
+		if (flag == 0 || numero < nMin)
+		{
+			*nMin = numero;
+			flag = 1;
+		}
+
+		++i;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
